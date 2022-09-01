802f39b7be6f1c67af4ae03f1bfb5d5a5af97bf8
vickom00 <vickomomola@gmail.com>
2022-09-01 10:27:36 -0400

    Added PostgreSQL database

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -10,3 +10,22 @@ resource "aws_db_instance" "default" {
  # parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
+
+resource "random_password" "password" {
+  length = 20
+  special = false
+  override_special = "_%@"
+}
+resource "aws_db_instance" "test_db" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  identifier           =  "dev-db"     
+  engine_version       = "13"
+  instance_class       = "db.t3.micro"
+  db_name              = "ntweeklydb001"
+  username             = "dbadmin1"
+  password             = random_password.password.result
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+
+}
